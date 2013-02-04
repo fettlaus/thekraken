9ea99a4808a58402a55a9a99933ed9c95a391547
Arne Wischer <Fettlaus@gmail.com>
2013-02-04 15:29:35 +0000

    EventBus as member

--- src/main/java/de/fettlaus/thekraken/model/MessageDispatcher.java
+++ src/main/java/de/fettlaus/thekraken/model/MessageDispatcher.java
@@ -7,9 +7,11 @@ import de.fettlaus.thekraken.events.EventBus;
 public class MessageDispatcher implements Runnable {
 
 	private final BlockingQueue<Message> messages;
-
+	EventBus evt;
+	
 	public MessageDispatcher(BlockingQueue<Message> messages) {
 		this.messages = messages;
+		evt = EventBus.instance();
 	}
 
 	@Override
@@ -18,7 +20,7 @@ public class MessageDispatcher implements Runnable {
 		while (true) {
 			try {
 				msg = messages.take();
-				EventBus.instance().post(msg);
+				evt.post(msg);
 			} catch (final InterruptedException e) {
 				e.printStackTrace();
 			}
