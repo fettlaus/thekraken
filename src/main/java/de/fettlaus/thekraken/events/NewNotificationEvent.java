19f1ce9ff79850a6e54f90aa49e8fbd5f3fb3803
Arne Wischer <Fettlaus@gmail.com>
2013-02-07 02:57:13 +0000

    added new notification types

--- src/main/java/de/fettlaus/thekraken/events/NewNotificationEvent.java
+++ src/main/java/de/fettlaus/thekraken/events/NewNotificationEvent.java
@@ -2,7 +2,12 @@ package de.fettlaus.thekraken.events;
 
 public class NewNotificationEvent implements ModelEvent {
 	public enum NotificationType {
-		NO_HOST_FOUND, CLOSING_ERROR, NEW_CONNECTION;
+		NO_HOST_FOUND, 
+		CLOSING_ERROR, 
+		NEW_CONNECTION, 
+		CANT_SEND_UDP, 
+		CANT_CONNECT_TO_HOST, 
+		UDP_ERROR;
 	}
 
 	NotificationType type;
