0f2d00298ef4b591bbd59a5850f4dfe5770f1f87
Arne Wischer <Fettlaus@gmail.com>
2013-02-10 20:54:39 +0000

    only one eventbus thread for now (problems with dispatching)

--- src/main/java/de/fettlaus/thekraken/events/EventBus.java
+++ src/main/java/de/fettlaus/thekraken/events/EventBus.java
@@ -4,7 +4,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class EventBus extends com.google.common.eventbus.AsyncEventBus {
-	private static EventBus instance = new EventBus( Executors.newFixedThreadPool(2));
+	private static EventBus instance = new EventBus( Executors.newFixedThreadPool(1));
 
 	public static EventBus instance() {
 		return instance;
